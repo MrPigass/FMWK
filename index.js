a879f1283e85c32549588bcb3691c627cb9e2f1e
Mr_Pigass <mrpigasss@gmail.com>
2023-03-13 15:16:55 +0100

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 /*https://www.intersport.ch/fr/mountain-hardwear-cloud-bank-gore-tex-lt-insulated-jacket-vetements-orange-ich.mountainhardwear.1942841.838.html?dwvar_ich.mountainhardwear.1942841.838_color=838&dwvar_ich.mountainhardwear.1942841.838_size=XL*/
-const inputElement = document.getElementById("form1");
-let previousValue = inputElement.value;
 let app = new Vue({
     el: '#app',
     data: {
+        currentIndex: 0,
         vestes: [
             {
                 id:1,
@@ -50,26 +49,39 @@ let app = new Vue({
 
     },
     methods:{
+        mouseover(index) {
+          
+            /* Affichage de l'index dans la console */
+            console.log(index)
+  
+            /* Mise en évidence de la zone sélectionnée => obsolete => voir CSS
+            event.target.style.backgroundColor = 'red'; */
+  
+            /* Changement d'index, afin que l'image change */
+            this.currentIndex = index;
+      },
+        /*ajouter au panier*/
         ajouterAuPanier(couleur){
             this.vestes.forEach(veste => {
                 if(veste.couleur == couleur){                   
                     if(this.compteNBVestesMMCouleur(couleur) < veste.quantite){
                         this.panier.push(veste);
+                        /*protéger le stock pour ne pas ajouter en plus*/
                     } else {
                         alert("Vous avez atteint la quantité maximale de cette veste");
                     }
                 }
             });
         },
-
+        /*supprimer du panier*/
         supprimerDuPanier(couleur){
-            this.panier.forEach(veste => {
-                if(veste.couleur == couleur){
-                    this.panier.splice(this.panier.indexOf(veste), 1);
-                }
-            });
+            const index = this.panier.findIndex(veste => veste.couleur === couleur);
+            if (index !== -1) {
+                this.panier.splice(index, 1);
+            }
         },
 
+        /*compter le nombre de veste dans le panier de la même couleur*/
         compteNBVestesMMCouleur(couleur){
             let nbVestes = 0;
             this.panier.forEach(veste => {
@@ -79,7 +91,7 @@ let app = new Vue({
             });
             return nbVestes;
         },
-
+        /*compter le prix total du panier*/
         totalPrix(){
             let total = 0;
             this.panier.forEach(veste => {
@@ -87,34 +99,28 @@ let app = new Vue({
             });
             return total;
         },
-        changeValeurVeste(couleur){
-            const currentValue = event.target.value;
-            if (currentValue > previousValue) {
-                this.ajouterAuPanier(couleur);
-              } else if (currentValue < previousValue) {
-                this.supprimerDuPanier(couleur);
-              }
-              previousValue = currentValue;
-        },
+
+        /*vider le panier*/
         viderPanier(){
             this.panier = [];
         },
+        /*calcule si il en reste 3 ou moins ou si il n'y a plus rien */
         epuisementStock(couleur){
-            /*si il ne reste plus que 2 veste/
-            /*et si il n'y a plus de veste*/
+             /*on récupère le nombre de veste restant dans le stock*/          
             nbvesteRestant=this.vestes.find(veste => veste.couleur == couleur).quantite
+            /*si il n'y a plus de veste*/
             if(nbvesteRestant-this.compteNBVestesMMCouleur(couleur) == 0){
                 return "en rupture de stock";
+            /*si il ne reste plus que 2 veste*/ 
             }else if(nbvesteRestant-this.compteNBVestesMMCouleur(couleur) <= 2){
                 return "presque épuisé";
             }else {
                 return "";
             }
         },
+        /*calcule si le client est premium ou non*/
         clientPremium(){
             return this.panier.length >= 4    
-        },
-
-
+        }
     }
 });
